66e70b94b9db3d597e200e4cc13558233a351299
pazworld <paz@moudamepo.net>
2021-03-14 14:21:27 +0900

    Red: Add test for all gutter game

---
+++ src/bowling_game_test.erl
@@ -0,0 +1,10 @@
+-module(bowling_game_test).
+-include_lib("eunit/include/eunit.hrl").
+
+gutter_game_test() ->
+    G = bowling_game:new(),
+    [G ! {roll, 0} || _ <- lists:seq(1, 20)],
+    G ! {score, self()},
+    receive
+        Score -> ?assertEqual(0, Score)
+    end.
