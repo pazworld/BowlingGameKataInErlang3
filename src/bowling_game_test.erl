3d261296a6c922db8906ff9ba0b9377d85cecaee
pazworld <paz@moudamepo.net>
2021-03-14 17:26:42 +0900

    Red: Add test for all ones game

--- src/bowling_game_test.erl
+++ src/bowling_game_test.erl
@@ -8,3 +8,11 @@ gutter_game_test() ->
     receive
         Score -> ?assertEqual(0, Score)
     end.
+
+all_ones_test() ->
+    G = bowling_game:new(),
+    [G ! {roll, 1} || _ <- lists:seq(1, 20)],
+    G ! {score, self()},
+    receive
+        Score -> ?assertEqual(20, Score)
+    end.
