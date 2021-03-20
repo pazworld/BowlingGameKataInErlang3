ea638aefd93d5bcc7ab749a530039acc0d741a41
pazworld <paz@moudamepo.net>
2021-03-21 00:20:56 +0900

    Misc: Add comment

--- src/bowling_game.erl
+++ src/bowling_game.erl
@@ -10,17 +10,22 @@ game(Rolls) ->
         {score, Sender} -> Sender ! score(1, lists:reverse(Rolls))
     end.
 
+% All rolls are processed
 score(_FrameIndex, []) ->
     0;
 
+% 10th frame
 score(10, Rolls) ->
     lists:sum(Rolls);
 
+% Strike
 score(FrameIndex, [Pins1, Pins2, Pins3 | Rest]) when Pins1 =:= 10 ->
     10 + Pins2 + Pins3 + score(FrameIndex + 1, [Pins2, Pins3 | Rest]);
 
+% Spare
 score(FrameIndex, [Pins1, Pins2, Pins3 | Rest]) when Pins1 + Pins2 =:= 10 ->
     10 + Pins3 + score(FrameIndex + 1, [Pins3 | Rest]);
 
+% Not strike nor spare
 score(FrameIndex, [Pins1, Pins2 | Rest]) ->
     Pins1 + Pins2 + score(FrameIndex + 1, Rest).
