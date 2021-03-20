22a0dbf3d6936164f7b5904335c7306bdcc80a04
pazworld <paz@moudamepo.net>
2021-03-20 22:11:02 +0900

    Green: It scores a game with spares

--- src/bowling_game.erl
+++ src/bowling_game.erl
@@ -7,5 +7,14 @@ new() ->
 game(Rolls) ->
     receive
         {roll, Pins} -> game([Pins | Rolls]);
-        {score, Sender} -> Sender ! lists:sum(Rolls)
+        {score, Sender} -> Sender ! score(lists:reverse(Rolls))
     end.
+
+score([]) ->
+    0;
+
+score([Pins1, Pins2, Pins3 | Rest]) when Pins1 + Pins2 =:= 10 ->
+    10 + Pins3 + score([Pins3 | Rest]);
+
+score([Pins1, Pins2 | Rest]) ->
+    Pins1 + Pins2 + score(Rest).
